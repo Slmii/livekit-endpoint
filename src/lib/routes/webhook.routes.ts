fdc35f154f8c6d3faa837e35bf9a615b1821e465
Slmii <selami.c@sbytes-it.com>
2024-07-24 16:06:26 +0200

    chore: Refactor webhook route to handle errors during event reception

--- src/lib/routes/webhook.routes.ts
+++ src/lib/routes/webhook.routes.ts
@@ -3,8 +3,12 @@ import express, { NextFunction, Request, Response } from 'express';
 const webhookRoutes = express.Router();
 
 webhookRoutes.post('/', async (req: Request<any, any, any>, res: Response, _next: NextFunction) => {
-	const event = req.receiver.receive(req.body, req.get('Authorization'));
-	console.log('Received event', event);
+	try {
+		const event = req.receiver.receive(req.body, req.get('Authorization'));
+		console.log('Received event', event.event);
+	} catch (error) {
+		console.log('Error receiving webhook', error);
+	}
 });
 
 export { webhookRoutes };
