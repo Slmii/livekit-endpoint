8a5e0879a1172afa3efa5b6ca10bf500f26b498e
Slmii <selami.c@sbytes-it.com>
2024-07-24 21:40:19 +0200

    add res

--- src/lib/routes/webhook.routes.js
+++ src/lib/routes/webhook.routes.js
@@ -3,12 +3,14 @@ import express from 'express';
 const webhookRoutes = express.Router();
 
 webhookRoutes.post('/', async (req, res, _next) => {
-	console.log('Received webhook', req.get('Authorization'));
 	try {
 		const event = await req.receiver.receive(req.body, req.get('Authorization'));
 		console.log('Received event', event.event);
+
+		res.status(200).send('Received');
 	} catch (error) {
 		console.log('Error receiving webhook', error);
+		res.status(500).send(error.message);
 	}
 });
 
