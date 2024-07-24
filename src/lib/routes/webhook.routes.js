bf8c94dd15c72836e5505ee90bc3e1ca7df071c2
Slmii <selami.c@sbytes-it.com>
2024-07-24 20:58:51 +0200

    remove body log

--- src/lib/routes/webhook.routes.js
+++ src/lib/routes/webhook.routes.js
@@ -3,7 +3,7 @@ import express from 'express';
 const webhookRoutes = express.Router();
 
 webhookRoutes.post('/', async (req, res, _next) => {
-	console.log('Received webhook', req.body, req.get('Authorization'));
+	console.log('Received webhook', req.get('Authorization'));
 	try {
 		const event = await req.receiver.receive(req.body, req.get('Authorization'));
 		console.log('Received event', event.event);
