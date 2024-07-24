32cae2decabfcd8dcb68db936d897dc692a02b2d
Slmii <selami.c@sbytes-it.com>
2024-07-24 16:54:31 +0200

    Refactor webhook route to handle event reception errors

--- src/lib/routes/webhook.routes.ts
+++ src/lib/routes/webhook.routes.ts
@@ -1,26 +1,11 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { jwtDecode } from 'jwt-decode';
 
 const webhookRoutes = express.Router();
 
 webhookRoutes.post('/', async (req: Request<any, any, any>, res: Response, _next: NextFunction) => {
 	try {
-		// const authToken = req.get('Authorization');
-		// if (!authToken) {
-		// 	return res.status(401).send({ msg: 'Unauthorized' });
-		// }
-
-		// const payload = jwtDecode(authToken);
-
-		// if (payload.exp) {
-		// 	const expiry = new Date(payload.exp * 1000);
-		// 	if (expiry < new Date()) {
-		// 		return res.status(401).send({ msg: 'Unauthorized' });
-		// 	}
-		// }
-
-		const event = req.body;
-		console.log('Received event', event);
+		const event = req.receiver.receive(req.body, req.get('Authorization'));
+		console.log('Received event', event.event);
 	} catch (error) {
 		console.log('Error receiving webhook', error);
 	}
